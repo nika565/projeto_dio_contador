a0cb9086053d4374727d3fd6ce9cd8294b1bd668
Nathan Barros <nathansousa0809@gmail.com>
2024-07-30 13:51:01 -0300

    projeto da DIO

---
+++ src/Contador.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = scanner.nextInt();
+
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = scanner.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch(ParametrosInvalidosException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+
+        if(parametroUm > parametroDois){
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for(var i = 0; i < contagem; i++){
+            System.out.println("Imprimindo o número: " + i);
+        }
+
+    }
+}
